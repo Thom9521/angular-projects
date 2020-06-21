831162aa2cf62a47ef1556e0aae5a23084a04b39
ThomasCloudpro <tmc@cloudpro.dk>
2020-06-21 11:48:39 +0200

    Template driven form

--- corona-api-learning/src/app/app.module.ts
+++ corona-api-learning/src/app/app.module.ts
@@ -38,9 +38,10 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { MatTableModule } from "@angular/material/table";
 import { MatSortModule } from "@angular/material/sort";
 import { MatPaginatorModule } from "@angular/material/paginator";
+import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [AppComponent, DialogOverviewExampleDialog],
+  declarations: [AppComponent, DialogOverviewExampleDialog, TestComponent],
   entryComponents: [DialogOverviewExampleDialog],
 
   imports: [
